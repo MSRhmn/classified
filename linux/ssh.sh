3b1038e6e30aa3b8660d307bc55c58ddc5fc66a3
Md. Shamim Rahman <apubdtea@gmail.com>
2023-10-09 17:23:33 +0600

    ssh file modiefied

--- linux/ssh.sh
+++ linux/ssh.sh
@@ -9,7 +9,11 @@ ssh-keygen -t ed25519 -C "username@email.com" # ID name ed25519 can be configura
 eval "$(ssh-agent -s)" # Evaluating ssh-agent by running this command.
 ssh-add ~/.ssh/id_ed25519 # Adding generated key to ssh-agent. Use only "ssh-add" to all generated key to ssh-agent. Only if there is multiple keys generated manually named.
 cat ~/.ssh/id_ed25519.pub # To check and copy the public key, then setting into the server.
-openssh-server # install it on desktop machine to use that machine over ssh.
 ssh <user@ip/host> # to ssh into a system.
-ssh -X <user@ip/host> # for X11 server based app use.
 scp file_name host_ip:/path/file_name # secure copy files via ssh.
+
+
+# ssh for desktop
+
+openssh-server # install it on desktop machine to use that machine over ssh.
+ssh -X <user@ip/host> # for X11 server based app use.
